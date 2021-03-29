b7b564d44508c4489bccaace5787d7bed6866aa5
Jarvis Yang <jarvisgyang@gmail.com>
2021-03-28 19:13:28 -0500

    Adding csstype to allow in-line styling.

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import CSS from 'csstype';
+
+const styles: CSS.Properties = {
+  background: 'lightblue',
+  border: '2px solid darkblue',
+  fontSize: '30px',
+  fontWeight: 'normal',
+  cursor: 'pointer',
+  outline: 'none',
+};
 
 const Square = ({ value, onClick }) => (
-  <button type='button' onClick={onClick}>
+  <button type='button' style={styles} onClick={onClick}>
     {value}
   </button>
 );
